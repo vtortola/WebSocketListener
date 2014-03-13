a8f9e8828792df39e703ab24ff0734fa5eb7cb14
vtortola <vtortola@gmail.com>
2014-03-13 00:53:36 +0000

    Echo load generator

--- TestApps/WebSocketLoadTest/WebSocketLoadTest/Program.cs
+++ TestApps/WebSocketLoadTest/WebSocketLoadTest/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Net.WebSockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,25 +12,35 @@ namespace WebSocketLoadTest
 {
     class Program
     {
+        static Boolean _partialFrames;
         static Byte[] msgOut;
         static ArraySegment<Byte> segmentOut;
+        static String _message = "This is a message example that it is supposed to contain little simulation data, but longh enough to simulate a very simple JSON object that needs to be sent to the client. I am kind of tired of this shit, i need to know why this is going slow damn it fuck tcpip programming.";
 
         static Int32 amount, interval;
         static Uri host;
         static void Main(string[] args)
         {
-            if (args == null || args.Length < 3 || String.IsNullOrWhiteSpace(args[0]) || !Uri.TryCreate("ws://" + args[0], UriKind.Absolute, out host) || !Int32.TryParse(args[1], out amount) || !Int32.TryParse(args[2], out interval))
+            if (args == null || args.Length < 3 || String.IsNullOrWhiteSpace(args[0]) || !Uri.TryCreate(args[0], UriKind.Absolute, out host) || !Int32.TryParse(args[1], out amount) || !Int32.TryParse(args[2], out interval))
             {
                 Console.WriteLine("Usage: [IP:Port] [Amount] [Interval ms]");
                 Console.ReadKey(true);
                 return;
             }
 
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidationCallback;
+
+            if (args.Length == 4 && args[3] == "p")
+            {
+                Console.WriteLine("Partial frames enabled");
+                _partialFrames = true;
+            }
+    
             Console.WriteLine("Clients: " + amount + ", interval: " + interval + " ms.");
 
-            msgOut = Encoding.UTF8.GetBytes("This is a message example that it is supposed to contain little simulation data, but longh enough to simulate a very simple JSON object that needs to be sent to the client. I am kind of tired of this shit, i need to know why this is going slow damn it fuck tcpip programming.");
+            msgOut = Encoding.UTF8.GetBytes(_message);
             segmentOut = new ArraySegment<Byte>(msgOut, 0, msgOut.Length);
-
+            Console.WriteLine(msgOut.Length.ToString() + " bytes");
             Int32 a, b;
             ThreadPool.GetMaxThreads(out a, out b);
             Console.WriteLine("max pool: " + a + "  , max IOCP: " + b);
@@ -48,7 +60,10 @@ namespace WebSocketLoadTest
             }
             catch(AggregateException aex)
             {
-                throw aex.GetBaseException();
+                var ex = aex.GetBaseException();
+                while (ex.InnerException != null)
+                    ex = ex.InnerException.GetBaseException();
+                throw ex;
             }
 
             Console.ReadKey(true);
@@ -77,10 +92,22 @@ namespace WebSocketLoadTest
 
                     while (!cancel.IsCancellationRequested)
                     {
-                        await client.SendAsync(segmentOut, WebSocketMessageType.Text, true, cancel).ConfigureAwait(false);
+                        var r = ran.Next();
+                        if (r % 5 == 0 && _partialFrames)
+                            await SendMessageInThreeParts(cancel, client);
+                        else if (r % 2 == 0 && _partialFrames)
+                            await SendMessageInTwoParts(cancel, client);
+                        else
+                            await SendMessageInOnePart(cancel, client);
+
                         var result = await client.ReceiveAsync(segmentIn, cancel).ConfigureAwait(false);
 
-                        await Task.Delay(interval, cancel).ConfigureAwait(false);
+                        String s = Encoding.UTF8.GetString(segmentIn.Array, segmentIn.Offset, result.Count);
+                        if (s != _message)
+                            throw new Exception("This is not what I sent: " + s);
+
+                        if(interval != 0)
+                            await Task.Delay(interval, cancel).ConfigureAwait(false);
                     }
                 }
                 catch(Exception aex)
@@ -89,12 +116,44 @@ namespace WebSocketLoadTest
                     {
                         var ex = aex.GetBaseException();
                         while(ex.InnerException != null)
-                            ex = ex.InnerException;
+                            ex = ex.InnerException.GetBaseException();
                         Console.WriteLine(DateTime.Now.ToString("dd/MM/yyy hh:mm:ss.fff ") + "(" + ex.GetType().Name + ") " + ex.Message);
                     }
+                    Thread.Sleep(3000);
                 }
             }
 
+        }
+
+        private static async Task SendMessageInThreeParts(CancellationToken cancel, ClientWebSocket client)
+        {
+            //Console.WriteLine("three");
+            var x = new ArraySegment<Byte>(msgOut, 0, 100);
+            var y = new ArraySegment<Byte>(msgOut, 100, 50);
+            var z = new ArraySegment<Byte>(msgOut, 150, msgOut.Length - 150);
+            await client.SendAsync(x, WebSocketMessageType.Text, false, cancel).ConfigureAwait(false);
+            await client.SendAsync(y, WebSocketMessageType.Text, false, cancel).ConfigureAwait(false);
+            await client.SendAsync(z, WebSocketMessageType.Text, true, cancel).ConfigureAwait(false);
+        }
+
+        private static async Task SendMessageInTwoParts(CancellationToken cancel, ClientWebSocket client)
+        {
+            //Console.WriteLine("two");
+            var x = new ArraySegment<Byte>(msgOut, 0, 100);
+            var z = new ArraySegment<Byte>(msgOut, 100, msgOut.Length - 100);
+            await client.SendAsync(x, WebSocketMessageType.Text, false, cancel).ConfigureAwait(false);
+            await client.SendAsync(z, WebSocketMessageType.Text, true, cancel).ConfigureAwait(false);
+        }
+
+        private static async Task SendMessageInOnePart(CancellationToken cancel, ClientWebSocket client)
+        {
+            //Console.WriteLine("one");
+            await client.SendAsync(segmentOut, WebSocketMessageType.Text, true, cancel).ConfigureAwait(false);
+        }
+
+        static bool RemoteCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
         }
     }
 }
