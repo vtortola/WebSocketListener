ab416cc130483f6c16fb12925096b271e901c6aa
vtortola <vtortola@gmail.com>
2014-03-27 01:00:18 +0000

    LINQ Rx Chat server sample

---
+++ samples/RxWebSocketChat/RxWebSocketChatServer/Program.cs
@@ -0,0 +1,121 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using vtortola.WebSockets;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellation = new CancellationTokenSource();
+
+            var endpoint = new IPEndPoint(IPAddress.Any, 8001);
+            WebSocketListener server = new WebSocketListener(endpoint, new WebSocketListenerOptions() { SubProtocols = new[]{"chat"} });
+            server.Start();
+
+            Log("Rx Chat Server started at " + endpoint.ToString());
+
+            var accept = Observable.FromAsync(server.AcceptWebSocketAsync)
+                                   .Select(ws =>
+                                   {
+                                       var inStream = Observable.FromAsync<WebSocketMessageReadStream>(ws.ReadMessageAsync)
+                                                                .DoWhile(()=>ws.IsConnected)
+                                                                .Where(reader=> reader != null)
+                                                                .Select(reader =>
+                                                                {
+                                                                    using (var sr = new StreamReader(reader, Encoding.UTF8, false, 8192, true))
+                                                                        return (dynamic)JObject.Load(new JsonTextReader(sr));
+                                                                });
+
+                                       JsonSerializer json = new JsonSerializer();
+                                 
+                                       var outStream = Observer.Create<dynamic>(value =>
+                                           {
+                                               using (var writer = ws.CreateMessageWriter(WebSocketMessageType.Text))
+                                               using (var sw = new StreamWriter(writer, Encoding.UTF8))
+                                                   json.Serialize(sw, value);
+                                           });
+                                      
+                                       return new ChatParticipant() { In= inStream, Out= outStream};
+                                   });
+
+            var connections = Observable.While(() => !cancellation.IsCancellationRequested, accept);
+
+            var rooms = new ConcurrentDictionary<String,List<ChatParticipant>>();
+
+            Action<ChatParticipant, dynamic> roomSubscriptor = (c,i) =>
+                {
+                    if (i.cls != null && i.cls == "join" && i.room != null)
+                    {
+                        String key = i.room;
+                        var room = rooms.GetOrAdd(key, new List<ChatParticipant>());
+                        room.Add(c);
+                        c.Nick = i.nick;
+                        Console.WriteLine("Room: " + key + " joined by " + c.Nick);
+                        i.participants = new JArray(room.Where(cc => cc.Nick != c.Nick).Select(x => x.Nick).ToArray());
+                        c.Out.OnNext(i);
+                        var anounce = new { cls = "msg", message = c.Nick + " joined the room.", room = key, nick = "Server", timestamp = DateTime.Now.ToString("hh:mm:ss") };
+                        foreach (var client in rooms[key])
+                        {
+                            if(client.Nick != c.Nick)
+                                client.Out.OnNext(new { cls="joint", room = key, nick = c.Nick });
+                            client.Out.OnNext(anounce);
+                        }
+                    }
+                };
+
+            Action<ChatParticipant,dynamic> messageSubscriptor = (c,i) =>
+                {
+                    if (i.cls != null && i.cls == "msg" && i.message != null && i.room != null)
+                    {
+                        String key = i.room;
+                        i.nick = c.Nick;
+                        i.timestamp = DateTime.Now.ToString("hh:mm:ss");
+                        Console.WriteLine("Message to Room: " + key);
+                        foreach (var client in rooms[key])
+                            client.Out.OnNext(i);
+                    }
+                };
+
+            connections.Do(c => 
+            {
+                c.In.Do(i=>roomSubscriptor(c,i))
+                    .Do(i=> messageSubscriptor(c,i))
+                    .Do(i => Console.WriteLine(i??"null"))
+                    .Subscribe();
+
+            }).Subscribe();
+         
+            Console.ReadKey(true);
+            Log("Server stoping");
+            cancellation.Cancel();
+            Console.ReadKey(true);
+        }
+
+        static void Log(String s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    public class ChatParticipant
+    {
+        public IObservable<dynamic> In { get; set; }
+        public IObserver<dynamic> Out { get; set; }
+        public String Nick { get; set; }
+    }
+
+}
---
+++ samples/RxWebSocketChat/WebSocketChatClientMVC/Content/Scripts/chat.js
@@ -0,0 +1,99 @@
+﻿var chat = function (control, url) {
+    var me = {};
+    var ws = new WebSocket(url, "chat");
+    var nickInput = control.getElementsByClassName('nick-input')[0];
+    var chatRoomInput = control.getElementsByClassName('chat-room-input')[0];
+    var joinRoom = control.getElementsByClassName('join-room')[0];
+    var chatLog = control.getElementsByClassName('chat-log')[0];
+    var message = control.getElementsByClassName('message')[0];
+    var send = control.getElementsByClassName('send')[0];
+    var chatParticipants = control.getElementsByClassName('chat-participants')[0];
+
+    message.disabled = true;
+    send.disabled = true;
+    joinRoom.disabled = true;
+
+    joinRoom.addEventListener('click', function () {
+
+        if (nickInput.value && chatRoomInput.value) {
+
+            joinRoom.disabled = true;
+            ws.send(JSON.stringify({ cls: "join", room: chatRoomInput.value, nick: nickInput.value }));
+        }
+        else {
+            if (!nickInput.value) {
+                if(nickInput.className.indexOf('error') == -1)
+                    nickInput.className += ' error';
+            }
+            if (!chatRoomInput.value) {
+                if (chatRoomInput.className.indexOf('error') == -1)
+                    chatRoomInput.className += ' error';
+            }
+        }
+
+    });
+
+    send.addEventListener('click', function () {
+        ws.send(JSON.stringify({ message: message.value, cls: "msg", room: chatRoomInput.value }));
+    });
+
+    var addChatMessage = function (nick, message, timestamp) {
+        var node = document.createElement('div');
+        node.className = 'chat-msg' +(nick == me.nick?' me':'');
+        node.textContent = timestamp + ': [' + nick + ']->   ' + message;
+        chatLog.appendChild(node);
+        chatLog.scrollTop = chatLog.scrollHeight;
+    };
+
+    var addParticipant = function (nick) {
+        var node = document.createElement('div');
+        node.className = 'participant';
+        node.textContent = nick;
+        chatParticipants.appendChild(node);
+    };
+
+    ws.onopen = function () {
+        joinRoom.disabled = false;
+        nickInput.disabled = false;
+        chatRoomInput.disabled = false;
+    };
+
+    ws.onmessage = function (msg) {
+        var json = JSON.parse(msg.data);
+        switch (json.cls) {
+            case 'join':
+                joinRoom.disabled = true;
+                send.disabled = false;
+                message.disabled = false;
+                nickInput.disabled = true;
+                chatRoomInput.disabled = true;
+                addParticipant(json.nick);
+                me.nick = json.nick;
+                for (var i = 0; i < json.participants.length; i++) {
+                    addParticipant(json.participants[i]);
+                }
+                break;
+            case 'msg':
+                addChatMessage(json.nick, json.message, json.timestamp);
+                break;
+            case 'joint':
+                addParticipant(json.nick);
+                break;
+        }
+    };
+
+    ws.onclose = function () {
+        joinRoom.disabled = true;
+        send.disabled = true;
+    };
+
+    return me;
+}
+
+
+chats = [];
+var elements = document.getElementsByClassName('chat');
+for (var i = 0; i < elements.length; i++) {
+    var node = elements[i];
+    chats.push(chat(node, node.dataset.websocket));
+}
