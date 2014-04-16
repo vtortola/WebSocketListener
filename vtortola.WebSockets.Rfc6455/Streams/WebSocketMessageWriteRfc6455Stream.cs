23a07828d5810e3a40810d20ca32ad5562d950f1
vtortola <vtortola@gmail.com>
2014-04-16 14:56:45 +0100

    Writting buffer refactoring

--- vtortola.WebSockets.Rfc6455/Streams/WebSocketMessageWriteRfc6455Stream.cs
+++ vtortola.WebSockets.Rfc6455/Streams/WebSocketMessageWriteRfc6455Stream.cs
@@ -32,20 +32,30 @@ namespace vtortola.WebSockets.Rfc6455
             ExtensionFlags.Rsv2 = extensionFlags.Rsv2;
             ExtensionFlags.Rsv3 = extensionFlags.Rsv3;
         }
-        private void CacheOrWrite(Byte[] buffer, ref Int32 offset, ref Int32 count)
+        private void BufferData(Byte[] buffer, ref Int32 offset, ref Int32 count)
         {
-            if (_internalUsedBufferLength != 0)
-            {
-                if (_internalUsedBufferLength < _internalBuffer.Count)
-                {
-                    var read = Math.Min(count, _internalBuffer.Count - _internalUsedBufferLength);
-                    Array.Copy(buffer, offset, _internalBuffer.Array, _internalBuffer.Offset + _internalUsedBufferLength, read);
-                    _internalUsedBufferLength += read;
-                    offset += read;
-                    count -= read;
-                }
+            var read = Math.Min(count, _internalBuffer.Count - _internalUsedBufferLength);
+            if (read == 0)
+                return;
+            Array.Copy(buffer, offset, _internalBuffer.Array, _internalBuffer.Offset + _internalUsedBufferLength, read);
+            _internalUsedBufferLength += read;
+            offset += read;
+            count -= read;
+        }
+        public override void Write(Byte[] buffer, Int32 offset, Int32 count)
+        {
+            if (_finished == 1)
+                throw new WebSocketException("The write stream has been already flushed or disposed.");
 
-                if (_internalUsedBufferLength == _internalBuffer.Count)
+            RemoveUTF8BOM(buffer, ref offset, ref count);
+            if (count == 0)
+                return;
+
+            while (count > 0)
+            {
+                BufferData(buffer, ref offset, ref count);
+
+                if (_internalUsedBufferLength == _internalBuffer.Count && count > 0)
                 {
                     _webSocket.Connection.WriteInternal(_internalBuffer, _internalUsedBufferLength, false, _headerSent, _messageType, ExtensionFlags);
                     _internalUsedBufferLength = 0;
@@ -53,73 +63,25 @@ namespace vtortola.WebSockets.Rfc6455
                 }
             }
         }
-        public override void Write(Byte[] buffer, Int32 offset, Int32 count)
-        {
-            if (Interlocked.CompareExchange(ref _finished, 1, 1) == 1)
-                throw new WebSocketException("The write stream has been already flushed or disposed.");
-
-            RemoveUTF8BOM(buffer, ref offset, ref count);
-            if (count == 0)
-                return;
-
-            CacheOrWrite(buffer, ref offset, ref count);
-
-            if (count == 0)
-                return;
-
-            _internalUsedBufferLength = Math.Min(count, _internalBuffer.Count);
-            count -= _internalUsedBufferLength;
-            Array.Copy(buffer, offset + count, _internalBuffer.Array, _internalBuffer.Offset, _internalUsedBufferLength);
-            offset += _internalUsedBufferLength;
-
-            if (count != 0)
-            {
-                var aux = new ArraySegment<Byte>(buffer, offset, count);
-                _webSocket.Connection.WriteInternal(aux, count, false, _headerSent, _messageType, ExtensionFlags);
-                _headerSent = true;
-            }
-        }
         public override async Task WriteAsync(Byte[] buffer, Int32 offset, Int32 count, CancellationToken cancellationToken)
         {
-            if (Interlocked.CompareExchange(ref _finished, 1, 1) == 1)
+            if (_finished == 1)
                 throw new WebSocketException("The write stream has been already flushed or disposed.");
 
             RemoveUTF8BOM(buffer, ref offset, ref count);
             if (count == 0)
                 return;
 
-            if (_internalUsedBufferLength != 0)
+            while (count > 0)
             {
-                if (_internalUsedBufferLength < _internalBuffer.Count)
-                {
-                    var read = Math.Min(count, _internalBuffer.Count - _internalUsedBufferLength);
-                    Array.Copy(buffer, offset, _internalBuffer.Array, _internalBuffer.Offset + _internalUsedBufferLength, read);
-                    _internalUsedBufferLength += read;
-                    offset += read;
-                    count -= read;
-                }
+                BufferData(buffer, ref offset, ref count);
 
-                if(_internalUsedBufferLength == _internalBuffer.Count)
+                if (_internalUsedBufferLength == _internalBuffer.Count && count > 0)
                 {
                     await _webSocket.Connection.WriteInternalAsync(_internalBuffer, _internalUsedBufferLength, false, _headerSent, _messageType, ExtensionFlags, cancellationToken);
                     _internalUsedBufferLength = 0;
                     _headerSent = true;
                 }
-            }
-
-            if (count == 0)
-                return;
-
-            _internalUsedBufferLength = Math.Min(count, _internalBuffer.Count);
-            count -= _internalUsedBufferLength;
-            Array.Copy(buffer, offset + count, _internalBuffer.Array, _internalBuffer.Offset, _internalUsedBufferLength);
-            offset += _internalUsedBufferLength;
-
-            if (count != 0)
-            {
-                var aux = new ArraySegment<Byte>(buffer, offset, count);
-                await _webSocket.Connection.WriteInternalAsync(aux, count, false, _headerSent, _messageType, ExtensionFlags, cancellationToken);
-                _headerSent = true;
             }
         }
         public override async Task FlushAsync(CancellationToken cancellationToken)
