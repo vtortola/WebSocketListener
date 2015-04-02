416ba11e7627645e8170880249870eaa648b3696
vtortola <vtortola@gmail.com>
2015-04-02 23:52:26 +0100

    Negotiation Queue & Strong name

---
+++ vtortola.WebSockets/Http/WebSocketNegotiationResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vtortola.WebSockets.Http
+{
+    public sealed class WebSocketNegotiationResult
+    {
+        public WebSocket Result { get; private set; }
+        public ExceptionDispatchInfo Error { get; private set; }
+        public WebSocketNegotiationResult(WebSocket result)
+        {
+            Result = result;
+        }
+        public WebSocketNegotiationResult(ExceptionDispatchInfo error)
+        {
+            Error = error;
+        }
+    }
+}
--- vtortola.WebSockets/WebSocketListener.cs
+++ vtortola.WebSockets/WebSocketListener.cs
@@ -6,23 +6,18 @@ using System.Runtime.ExceptionServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
+using vtortola.WebSockets.Http;
 using vtortola.WebSockets.Tools;
 
 namespace vtortola.WebSockets
 {
     public sealed class WebSocketListener: IDisposable
     {
-        sealed class WebSocketNegotiationResult
-        {
-            public WebSocket Result;
-            public ExceptionDispatchInfo Error;
-        }
-
         readonly TcpListener _listener;
-        readonly TransformBlock<Socket, WebSocketNegotiationResult> _negotiationQueue;
+        readonly HttpNegotiationQueue _negotiationQueue;
         readonly CancellationTokenSource _cancel;
         readonly WebSocketListenerOptions _options;
-        readonly WebSocketHandshaker _handShaker;
+        
         Boolean _isDisposed;
 
         public Boolean IsStarted { get; private set; }
@@ -46,10 +41,8 @@ namespace vtortola.WebSockets
 
             ConnectionExtensions = new WebSocketConnectionExtensionCollection(this);
             Standards = new WebSocketFactoryCollection(this);
-            Func<Socket, Task<WebSocketNegotiationResult>> negotiate = NegotiateWebSocket;
-            _negotiationQueue = new TransformBlock<Socket, WebSocketNegotiationResult>(negotiate, new ExecutionDataflowBlockOptions() { CancellationToken = _cancel.Token, MaxDegreeOfParallelism = options.ParallelNegotiations, BoundedCapacity = options.NegotiationQueueCapacity });
 
-            _handShaker = new WebSocketHandshaker(Standards, _options);
+            _negotiationQueue = new HttpNegotiationQueue(Standards, ConnectionExtensions, options);
         }
         public WebSocketListener(IPEndPoint endpoint)
             :this(endpoint,new WebSocketListenerOptions())
@@ -61,15 +54,13 @@ namespace vtortola.WebSockets
             {
                 var client = await _listener.AcceptSocketAsync().ConfigureAwait(false);
                 if (client != null)
-                    if (!_negotiationQueue.Post(client))
-                        FinishSocket(client);
+                {
+                    ConfigureSocket(client);
+                    _negotiationQueue.Queue(client);
+                }
             }
         }
-        private void FinishSocket(Socket client)
-        {
-            try { client.Dispose(); }
-            catch { }
-        }
+
         public void Start()
         {
             if (!_isDisposed)
@@ -98,50 +89,13 @@ namespace vtortola.WebSockets
             client.SendTimeout = (Int32)Math.Round(_options.WebSocketSendTimeout.TotalMilliseconds);
             client.ReceiveTimeout = (Int32)Math.Round(_options.WebSocketReceiveTimeout.TotalMilliseconds);
         }
-        private async Task<WebSocketNegotiationResult> NegotiateWebSocket(Socket client)
-        {
-            var result = new WebSocketNegotiationResult();
-            try
-            {
-                var timeoutTask = Task.Delay(_options.NegotiationTimeout);
-                ConfigureSocket(client);
 
-                Stream stream = new NetworkStream(client, FileAccess.ReadWrite, true);
-                foreach (var conExt in ConnectionExtensions)
-                {
-                    var extTask = conExt.ExtendConnectionAsync(stream);
-                    await Task.WhenAny(timeoutTask, extTask).ConfigureAwait(false);
-                    if (timeoutTask.IsCompleted)
-                        throw new WebSocketException("Negotiation timeout (Extension: "+conExt.GetType().Name+")");
-
-                    stream = await extTask;
-                }
-
-                var handshakeTask = _handShaker.HandshakeAsync(stream);
-                await Task.WhenAny(timeoutTask, handshakeTask).ConfigureAwait(false);
-                if (timeoutTask.IsCompleted)
-                    throw new WebSocketException("Negotiation timeout");
-
-                var handshake = await handshakeTask;
-
-                if (handshake.IsValid)
-                    result.Result = handshake.Factory.CreateWebSocket(stream, _options, (IPEndPoint)client.LocalEndPoint, (IPEndPoint)client.RemoteEndPoint, handshake.Request, handshake.Response, handshake.NegotiatedMessageExtensions);
-                else
-                    result.Error = handshake.Error;
-            }
-            catch (Exception ex)
-            {
-                FinishSocket(client);
-                result.Error = ExceptionDispatchInfo.Capture(ex);
-                result.Result = null;
-            }
-            return result;
-        }
         public async Task<WebSocket> AcceptWebSocketAsync(CancellationToken token)
         {
             try
             {
-                var result = await _negotiationQueue.ReceiveAsync(token).ConfigureAwait(false);
+                var result = await _negotiationQueue.DequeueAsync(token).ConfigureAwait(false);
+
                 if (result.Error != null)
                 {
                     result.Error.Throw();
@@ -165,7 +119,7 @@ namespace vtortola.WebSockets
                 this.Stop();
                 _listener.Server.Dispose();
                 _cancel.Cancel();
-                _negotiationQueue.Complete();
+                _negotiationQueue.Dispose();
                 _cancel.Dispose();
             }
         }
