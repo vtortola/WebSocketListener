8d90e82988ad74f9b96f0d76c47d3df6b0d309c8
vtortola <vtortola@gmail.com>
2015-11-08 03:10:48 +0000

    SSL RemoteCertificateValidationCallback

--- vtortola.WebSockets/Extensibility/WebSocketSecureConnectionExtension.cs
+++ vtortola.WebSockets/Extensibility/WebSocketSecureConnectionExtension.cs
@@ -9,23 +9,30 @@ namespace vtortola.WebSockets
     public sealed class WebSocketSecureConnectionExtension : IWebSocketConnectionExtension
     {
         readonly X509Certificate2 _certificate;
+        readonly RemoteCertificateValidationCallback _validation;
 
         public WebSocketSecureConnectionExtension(X509Certificate2 certificate)
         {
             _certificate = certificate;
         }
 
+        public WebSocketSecureConnectionExtension(X509Certificate2 certificate, RemoteCertificateValidationCallback validation)
+        {
+            _certificate = certificate;
+            _validation = validation;
+        }
+
         public Stream ExtendConnection(Stream stream)
         {
-            var ssl = new SslStream(stream, false);
-            ssl.AuthenticateAsServer(_certificate, false, SslProtocols.Default, false);
+            var ssl = new SslStream(stream, false, _validation);
+            ssl.AuthenticateAsServer(_certificate, _validation != null, SslProtocols.Default, false);
             return ssl;
         }
 
         public async Task<Stream> ExtendConnectionAsync(Stream stream)
         {
-            var ssl = new SslStream(stream, false);
-            await ssl.AuthenticateAsServerAsync(_certificate, false, SslProtocols.Default, false).ConfigureAwait(false);
+            var ssl = new SslStream(stream, false, _validation);
+            await ssl.AuthenticateAsServerAsync(_certificate, _validation != null, SslProtocols.Default, false).ConfigureAwait(false);
             return ssl;
         }
     }
