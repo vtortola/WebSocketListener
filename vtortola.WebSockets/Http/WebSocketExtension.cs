28a1fe5957d24f274ecb10b1f780ac853a3e7640
Denis Zykov <deniszykov@gmail.com>
2017-05-11 23:30:01 +0300

    split WebSocketExtension and WebSocketExtensionOptions in two files

--- vtortola.WebSockets/Http/WebSocketExtension.cs
+++ vtortola.WebSockets/Http/WebSocketExtension.cs
@@ -1,32 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace vtortola.WebSockets
 {
     public class WebSocketExtension
     {
-        public String Name { get; private set; }
+        public string Name { get; private set; }
         public IReadOnlyList<WebSocketExtensionOption> Options { get; private set; }
 
         static readonly ReadOnlyCollection<WebSocketExtensionOption> _empty = new ReadOnlyCollection<WebSocketExtensionOption>(new List<WebSocketExtensionOption>());
 
-        public WebSocketExtension(String name, List<WebSocketExtensionOption> options)
+        public WebSocketExtension(string name, List<WebSocketExtensionOption> options)
         {
             Name = name;
             Options = new ReadOnlyCollection<WebSocketExtensionOption>(options);
         }
-        public WebSocketExtension(String name)
+        public WebSocketExtension(string name)
         {
             Name = name;
             Options = _empty;
         }
-    }
-
-    public class WebSocketExtensionOption
-    {
-        public String Name { get; set; }
-        public String Value { get; set; }
-        public Boolean ClientAvailableOption { get; set; }
     }
 }
---
+++ vtortola.WebSockets/Http/WebSocketExtensionOption.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace vtortola.WebSockets {
+    public class WebSocketExtensionOption
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public bool ClientAvailableOption { get; set; }
+
+        public WebSocketExtensionOption(string name)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            this.Name = name;
+        }
+        public WebSocketExtensionOption(string name, bool clientAvailableOption)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            this.Name = name;
+            this.ClientAvailableOption = clientAvailableOption;
+        }
+        public WebSocketExtensionOption(string name, string value)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            this.Name = name;
+            this.Value = value;
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            if (this.ClientAvailableOption)
+                return this.Name;
+            else
+                return $"{this.Name}={this.Value}";
+        }
+    }
+}
