4851dbfba2889c4a9772e35bdcefee165f1e80d5
Denis Zykov <deniszykov@gmail.com>
2017-05-12 12:04:36 +0300

    WebSocketClient tests

---
+++ Tests/WebSocketListener.UnitTests/WebSocketClientTest.cs
@@ -0,0 +1,18 @@
+﻿using vtortola.WebSockets;
+using vtortola.WebSockets.Rfc6455;
+using Xunit;
+
+namespace WebSocketListener.UnitTests
+{
+    public class WebSocketClientTest
+    {
+        [Fact]
+        public void ConstructTest()
+        {
+            var factories = new WebSocketFactoryCollection()
+                .RegisterRfc6455();
+            var options = new WebSocketListenerOptions();
+            var webSocketClient = new WebSocketClient(factories, options);
+        }
+    }
+}
