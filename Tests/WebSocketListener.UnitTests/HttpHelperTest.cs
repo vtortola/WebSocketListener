67a90df5e841b5c64f4d482570988e3a66fd2016
Denis Zykov <deniszykov@gmail.com>
2017-05-12 10:02:31 +0300

    added http helper for HTTP parsing purpose

---
+++ Tests/WebSocketListener.UnitTests/HttpHelperTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using vtortola.WebSockets.Tools;
+using Xunit;
+
+namespace WebSocketListener.UnitTests
+{
+    public class HttpHelperTest
+    {
+        [Theory,
+        InlineData("HTTP/1.1 101 Web Socket Protocol Handshake", HttpStatusCode.SwitchingProtocols, "Web Socket Protocol Handshake"),
+        InlineData("HTTP/1.0 200 OK", HttpStatusCode.OK, "OK"),
+        InlineData("HTTP/1.1 200 OK", HttpStatusCode.OK, "OK"),
+        InlineData("HTTP/1.1 404 Not Found", HttpStatusCode.NotFound, "Not Found"),
+        InlineData(" HTTP/1.1 404 Not Found", HttpStatusCode.NotFound, "Not Found"),
+        InlineData("  HTTP/1.1 404 Not Found", HttpStatusCode.NotFound, "Not Found"),
+        InlineData(" HTTP/1.1  404 Not Found", HttpStatusCode.NotFound, "Not Found"),
+        InlineData(" HTTP/1.1   404 Not Found", HttpStatusCode.NotFound, "Not Found"),
+        InlineData(" HTTP/1.1   404  Not Found", HttpStatusCode.NotFound, "Not Found"),
+        InlineData(" HTTP/1.1   404   Not Found", HttpStatusCode.NotFound, "Not Found"),
+        InlineData(" HTTP/1.1   404   Not Found ", HttpStatusCode.NotFound, "Not Found"),
+        InlineData(" HTTP/1.1   404   Not Found  ", HttpStatusCode.NotFound, "Not Found"),
+        InlineData("HTTP/1.1 200", HttpStatusCode.OK, ""),
+        InlineData("HTTP/1.1 ", (HttpStatusCode)0, "Missing Response Code"),
+        InlineData("HTTP/1.1", (HttpStatusCode)0, "Missing Response Code"),
+        InlineData("HTTP/1", (HttpStatusCode)0, "Malformed Response"),
+        InlineData("", (HttpStatusCode)0, "Malformed Response")]
+        public void TryParseAndAddRequestHeaderTest(string headline, HttpStatusCode statusCode, string description)
+        {
+            var actualStatusCode = default(HttpStatusCode);
+            var actualDescription = default(string);
+            HttpHelper.TryParseHttpResponse(headline, out actualStatusCode, out actualDescription);
+
+            Assert.Equal(statusCode, actualStatusCode);
+            Assert.Equal(description, actualDescription);
+        }
+    }
+}
---
+++ vtortola.WebSockets/Tools/HttpHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+
+namespace vtortola.WebSockets.Tools
+{
+    public static class HttpHelper
+    {
+        public static readonly string WebSocketHttp10Version = "HTTP/1.0";
+        public static readonly string WebSocketHttp11Version = "HTTP/1.1";
+
+        public static bool TryParseHttpResponse(string headline, out HttpStatusCode statusCode, out string statusCodeDescription)
+        {
+            if (headline == null) throw new ArgumentNullException(nameof(headline));
+
+            var headlineStartIndex = 0;
+            var headlineLength = headline.Length;
+            HeadersHelper.TrimInPlace(headline, ref headlineStartIndex, ref headlineLength);
+
+            if (string.CompareOrdinal(headline, headlineStartIndex, WebSocketHttp11Version, 0, WebSocketHttp11Version.Length) != 0 &&
+                string.CompareOrdinal(headline, headlineStartIndex, WebSocketHttp10Version, 0, WebSocketHttp11Version.Length) != 0)
+            {
+                statusCode = 0;
+                statusCodeDescription = "Malformed Response";
+                return false;
+            }
+
+            var responseCodeStartIndex = Math.Min(headlineStartIndex + WebSocketHttp11Version.Length + 1, headline.Length);
+            HeadersHelper.Skip(headline, ref responseCodeStartIndex, UnicodeCategory.SpaceSeparator);
+            var responseCodeEndIndex = responseCodeStartIndex;
+            HeadersHelper.Skip(headline, ref responseCodeEndIndex, UnicodeCategory.DecimalDigitNumber);
+
+            if (responseCodeEndIndex == responseCodeStartIndex)
+            {
+                statusCode = 0;
+                statusCodeDescription = "Missing Response Code";
+                return false;
+            }
+            var responseStatus = ushort.Parse(headline.Substring(responseCodeStartIndex, responseCodeEndIndex - responseCodeStartIndex));
+
+            var descriptionStartIndex = responseCodeEndIndex;
+            var descriptionLength = headline.Length - descriptionStartIndex;
+
+            HeadersHelper.TrimInPlace(headline, ref descriptionStartIndex, ref descriptionLength);
+
+            statusCode = (HttpStatusCode)responseStatus;
+            statusCodeDescription = descriptionLength > 0 ? headline.Substring(descriptionStartIndex, descriptionLength) : string.Empty;
+            return true;
+        }
+    }
+}
