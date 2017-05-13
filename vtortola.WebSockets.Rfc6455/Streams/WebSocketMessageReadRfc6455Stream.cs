d46ff5e629aad79a644e4e5ab3f7aa96208c7b15
Denis Zykov <deniszykov@gmail.com>
2017-05-13 22:44:38 +0300

    refactored Read, ReadAsync methods

--- vtortola.WebSockets.Rfc6455/Streams/WebSocketMessageReadRfc6455Stream.cs
+++ vtortola.WebSockets.Rfc6455/Streams/WebSocketMessageReadRfc6455Stream.cs
@@ -7,7 +7,7 @@ namespace vtortola.WebSockets.Rfc6455
     public class WebSocketMessageReadRfc6455Stream : WebSocketMessageReadStream
     {
         readonly WebSocketRfc6455 _webSocket;
-        Boolean _hasPendingFrames;
+        bool _hasPendingFrames;
         readonly WebSocketMessageType _messageType;
         readonly WebSocketExtensionFlags _flags;
         public override WebSocketMessageType MessageType { get { return _messageType; } }
@@ -15,7 +15,7 @@ namespace vtortola.WebSockets.Rfc6455
 
         public WebSocketMessageReadRfc6455Stream(WebSocketRfc6455 webSocket)
         {
-            Guard.ParameterCannotBeNull(webSocket, "webSocket");
+            Guard.ParameterCannotBeNull(webSocket, nameof(webSocket));
 
             _webSocket = webSocket;
             _messageType = (WebSocketMessageType)_webSocket.Connection.CurrentHeader.Flags.Option;
@@ -34,80 +34,100 @@ namespace vtortola.WebSockets.Rfc6455
             return flags;
         }
 
-        private Int32 CheckBoundaries(Byte[] buffer, Int32 offset, Int32 count)
+        public override int Read(byte[] buffer, int offset, int count)
         {
-            if (count > buffer.Length - offset)
-                throw new ArgumentException("There is not space in the array for that length considering that offset.");
+            if (buffer == null) throw new ArgumentNullException(nameof(buffer));
+            if (offset < 0 || offset >= buffer.Length) throw new ArgumentOutOfRangeException(nameof(offset));
+            if (count < 0 || offset + count > buffer.Length) throw new ArgumentOutOfRangeException(nameof(count));
 
-            if (_webSocket.Connection.CurrentHeader == null)
+            if (count == 0)
                 return 0;
 
-            if (_webSocket.Connection.CurrentHeader.ContentLength < (Int64)count)
-                count = (Int32)_webSocket.Connection.CurrentHeader.ContentLength;
-
-            if (_webSocket.Connection.CurrentHeader.RemainingBytes < (Int64)count)
-                count = (Int32)_webSocket.Connection.CurrentHeader.RemainingBytes;
-
-            return count;
-        }
-
-        public override Int32 Read(Byte[] buffer, Int32 offset, Int32 count)
-        {
-            Int32 readed = 0;
+            var read = 0;
             do
             {
                 if (!_webSocket.IsConnected)
                     break;
 
-                var checkedcount = CheckBoundaries(buffer, offset, count);
-
-                if (checkedcount == 0 && !_hasPendingFrames)
+                var bytesToRead = this.GetBytesToRead(this._webSocket.Connection.CurrentHeader, count);
+                if (bytesToRead == 0 && !_hasPendingFrames)
                 {
                     _webSocket.Connection.DisposeCurrentHeaderIfFinished();
                     break;
                 }
-                else if (checkedcount == 0 && _hasPendingFrames)
+                else if (bytesToRead == 0 && _hasPendingFrames)
+                {
                     LoadNewHeader();
+                }
                 else
                 {
-                    readed = _webSocket.Connection.ReadInternal(buffer, offset, checkedcount);
+                    read = _webSocket.Connection.ReadInternal(buffer, offset, bytesToRead);
+
+                    offset += read;
+                    count -= read;
+
                     _webSocket.Connection.DisposeCurrentHeaderIfFinished();
+
                     if (_webSocket.Connection.CurrentHeader == null && _hasPendingFrames)
                         LoadNewHeader();
                 }
-            } while (readed == 0 && _webSocket.Connection.CurrentHeader.RemainingBytes != 0);
+            } while (read == 0 && _webSocket.Connection.CurrentHeader.RemainingBytes != 0 && count > 0);
 
-            return readed;
+            return read;
         }
 
-        public override async Task<Int32> ReadAsync(Byte[] buffer, Int32 offset, Int32 count, CancellationToken cancellationToken)
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
         {
-            Int32 readed = 0;
+            if(buffer == null) throw new ArgumentNullException(nameof(buffer));
+            if(offset < 0 || offset >= buffer.Length) throw new ArgumentOutOfRangeException(nameof(offset));
+            if(count < 0 || offset + count > buffer.Length) throw new ArgumentOutOfRangeException(nameof(count));
+
+            if (count == 0)
+                return 0;
+
+            var read = 0;
             do
             {
                 if (!_webSocket.IsConnected || cancellationToken.IsCancellationRequested)
                     break;
 
-                var checkedcount = CheckBoundaries(buffer, offset, count);
-
-                if (checkedcount == 0 && !_hasPendingFrames)
+                var bytesToRead = this.GetBytesToRead(this._webSocket.Connection.CurrentHeader, count);
+                if (bytesToRead == 0 && !_hasPendingFrames)
                 {
                     _webSocket.Connection.DisposeCurrentHeaderIfFinished();
                     break;
                 }
-                else if (checkedcount == 0 && _hasPendingFrames)
+                else if (bytesToRead == 0 && _hasPendingFrames)
+                {
                     await LoadNewHeaderAsync(cancellationToken).ConfigureAwait(false);
+                }
                 else
                 {
-                    readed = await _webSocket.Connection.ReadInternalAsync(buffer, offset, checkedcount, cancellationToken).ConfigureAwait(false);
+                    read = await _webSocket.Connection.ReadInternalAsync(buffer, offset, bytesToRead, cancellationToken).ConfigureAwait(false);
+
+                    offset += read;
+                    count -= read;
+
                     _webSocket.Connection.DisposeCurrentHeaderIfFinished();
+
                     if (_webSocket.Connection.CurrentHeader == null && _hasPendingFrames)
                         await LoadNewHeaderAsync(cancellationToken).ConfigureAwait(false);
                 }
-            } while (readed == 0 && _webSocket.Connection.CurrentHeader.RemainingBytes != 0);
+            } while (read == 0 && _webSocket.Connection.CurrentHeader.RemainingBytes != 0 && count > 0);
 
-            return readed;
+            return read;
         }
+
+        private int GetBytesToRead(WebSocketFrameHeader header, int bufferSize)
+        {
+            if (bufferSize <= 0) throw new ArgumentOutOfRangeException(nameof(bufferSize));
+
+            if (header == null)
+                return 0;
+
+            return (int)Math.Min(bufferSize, header.RemainingBytes);
+        }
+
 
         private void LoadNewHeader()
         {
