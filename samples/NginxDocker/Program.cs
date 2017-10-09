ac97ef5d13ff88e89d2f8a118fa06f25d705e40a
Val <vtortola@gmail.com>
2017-10-09 20:19:08 +0200

    Nginx with static files

---
+++ samples/NginxDocker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace NginxDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while(true)
+            {
+                Console.WriteLine("Hello World!");
+                //Thread.Sleep(5000);
+            }
+            
+        }
+    }
+}
